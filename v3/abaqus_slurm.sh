7dfc90965c733c019afa5b0448a54a06a4ffbfda
Reuben Kraft <reuben.kraft@gmail.com>
2024-12-05 20:12:15 -0500

    Update abaqus_slurm.sh

--- v3/abaqus_slurm.sh
+++ v3/abaqus_slurm.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
- 
+  
 #### specify the job and project name
 #SBATCH --job-name=abaqus
 #SBATCH --account=rhk12
- 
+
 #### specify the required resources
 #SBATCH --account=open
 #SBATCH --partition=open
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=4
-#SBATCH --mem=128GB
-#SBATCH --time=03:00:00
- 
+#SBATCH --nodes=1                 # Number of nodes
+#SBATCH --ntasks-per-node=1       # Number of tasks per node (MPI processes)
+#SBATCH --mem=128GB               # Total memory allocation
+#SBATCH --time=30:00:00           # Job time limit (hh:mm:ss)
+
+# Load necessary modules
 source ~/.bashrc
-module load abaqus
+module load abaqus/2022
+
 # Change to the directory containing the job file
 cd $SLURM_SUBMIT_DIR
 
-abaqus job=Job-1 input=Job-1.inp cpus=`nproc` mp_mode=threads interactive
+# Ensure Abaqus uses the correct number of CPUs
+total_cpus=$SLURM_NTASKS  # Total tasks (ntasks-per-node * nodes)
+
+# Run the Abaqus job using MPI
+abaqus job=Job-1 input=Job-1.inp cpus=$total_cpus mp_mode=mpi interactive
